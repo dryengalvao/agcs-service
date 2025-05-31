3f45bdbbdc9b4ff912c7fbd54c3cecdeea5fa7cc
Dryen Galvao <dryenhanzo@gmail.com>
2025-05-31 17:23:42 -0300

    Implementa Spring Actuator para verificação de status da aplicação.
    Adiciona a dependência do Spring Actuator para fornecer informações sobre o status atual da aplicação por meio da chamada do endpoint /api/health

--- src/main/java/br/com/agcs/controller/HealthController.java
+++ src/main/java/br/com/agcs/controller/HealthController.java
@@ -1,16 +1,28 @@
 package br.com.agcs.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/health")
+@RequestMapping("/api")
 public class HealthController {
+	
+	@Autowired
+	private Map<String, HealthIndicator> indicators;
 
-	@GetMapping
-	public ResponseEntity<String> healthCheck() {
-	    return ResponseEntity.ok("OK");
+	@GetMapping("/health")
+	public ResponseEntity<Map<String, Object>> statusHelth() {
+	    Map<String, Object> result = new HashMap<>();
+	    indicators.forEach((name, indicator) -> {
+	        result.put(name, indicator.health());
+	    });
+	    return ResponseEntity.ok(result);
 	}
 }
